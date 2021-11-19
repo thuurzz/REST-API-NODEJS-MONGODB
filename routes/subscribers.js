eabacd8d7d01fa7d9e99fa8540532b4b3afb7e43
Arthur Vinicius <thuur.vss@gmail.com>
2021-11-19 14:41:07 -0300

    Criação do modelo para o bd, criação do método post e get.

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,17 +1,32 @@
 const express = require("express")
 const router = express.Router()
-module.exports = router
+const Subscriber = require("../models/subscriber")
 
 // find all
-router.get("/", (req, res)=>{
-
+router.get("/", async (req, res)=>{
+    try {
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
 // find one
 router.get("/:id", (req, res)=>{
 })
 // update complete
-router.post("/", (req, res)=>{
+router.post("/", async (req, res)=>{
+    const subscriber = new Subscriber({
+        userName: req.body.userName,
+        userChannel: req.body.userChannel
+    })
 
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
 // update parcial
 router.patch("/:id", (req, res)=>{
@@ -21,3 +36,5 @@ router.patch("/:id", (req, res)=>{
 router.delete("/:id", (req, res)=>{
 
 })
+
+module.exports = router
